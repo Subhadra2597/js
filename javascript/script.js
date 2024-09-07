f80fce532837d5213f07b3e888039ddf0f867723
Subhadra2597 <subhadraanju@gmail.com>
2024-09-07 17:46:43 +0530

    assignment2

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,26 @@
-console.log("Hello")
-let a=11
-for(let i=0; i<a; i++)
-{
-    if(i%2==0)
-    {
-        console.log(i)
-    }
     
+    function even()
+    {
+
+
+        let a=11
+        for(let i=0; i<a; i++)
+        {
+            if(i%2==0)
+            {
+                console.log(i)
+            }
+            
+        }
+    }
+even()
+let multiple= (a,b) =>
+{
+    return(a*b)
+}
+console.log(multiple(5,6))
+let products=["Laptop","Keyboard","Mouse"]
+for(let productIndex=0; productIndex<products.length; productIndex++)
+{
+    document.write("Name- "+products[productIndex] +"\n")
 }
